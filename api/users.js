77f5b93267ea8741e0c3ab05ce9d01559a4b746b
laura piccioli <laura.piccioli@yahoo.com>
2026-02-09 21:37:53 -0500

    api/users

--- api/users.js
+++ api/users.js
@@ -8,9 +8,9 @@ import { createToken } from "#utils/jwt";
 
 router
   .route("/register")
-  .post(requireBody(["email", "password"]), async (req, res) => {
-    const { email, password } = req.body;
-    const user = await createUser({ email, password });
+  .post(requireBody(["username", "password"]), async (req, res) => {
+    const { username, password } = req.body;
+    const user = await createUser({ username, password });
 
     const token = await createToken({ id: user.id });
     res.status(201).send(token);
@@ -18,10 +18,10 @@ router
 
 router
   .route("/login")
-  .post(requireBody(["email", "password"]), async (req, res) => {
-    const { email, password } = req.body;
-    const user = await getUserByEmailAndPassword({ email, password });
-    if (!user) return res.status(401).send("Invalid email or password.");
+  .post(requireBody(["username", "password"]), async (req, res) => {
+    const { username, password } = req.body;
+    const user = await getUserByUsernameAndPassword({ username, password });
+    if (!user) return res.status(401).send("Invalid username or password.");
 
     const token = await createToken({ id: user.id });
     res.send(token);
